57034c4e998d5351e88eb4676c3b6c88da7834b2
Jair Avilés <hi@jairaviles.mx>
2018-08-02 22:47:40 -0700

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'platzisquare';
+  title = 'PlatziSquare';
+  places: any = [
+    {
+      active: true,
+      name: 'Floreria la Gardenia'
+    },
+    {
+      active: true,
+      name: 'Donas DunkiDonuts'
+    },
+    {
+      active: true,
+      name: 'Veterinaria Camila y Hachi'
+    },
+    {
+      active: false,
+      name: 'Panda Express'
+    },
+    {
+      active: true,
+      name: 'Supermarket Safeway'
+    },
+    {
+      active: true,
+      name: 'Convience store Ikea'
+    }
+  ];
+  lat: Number = 37.757815;
+  lng: Number = -122.5076404;
+
+  constructor() {}
+
 }
